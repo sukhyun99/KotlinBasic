6c50cc9a3d4db0b33ea18843cb3bff803880c9e7
weekendHuman <buddah99@naver.com>
2022-05-21 11:08:41 +0900

    토스트 사용

--- app/src/main/java/com/example/kotlinbasic/MainActivity.kt
+++ app/src/main/java/com/example/kotlinbasic/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlinbasic
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,10 @@ class MainActivity : AppCompatActivity() {
 
         btnLog.setOnClickListener {
             Log.d("메인화면", "로그 버튼이 클릭 됨")
+        }
+
+        btnToast.setOnClickListener {
+            Toast.makeText(this, "토스트 띄우기", Toast.LENGTH_SHORT).show()
         }
     }
 }
